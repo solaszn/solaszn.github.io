3419f7ab9b2b052761b526632af885f49799f41e
solaszn <olawuyi.sola@yahoo.com>
2025-01-14 23:20:05 +0100

    case study page

---
+++ astro-pop/src/components/contentKit.tsx
@@ -0,0 +1,27 @@
+// contentKit.tsx
+import Image from "astro/components/Image.astro";
+import React from 'react';
+
+interface ImageProps extends React.HTMLAttributes<HTMLImageElement> {
+  data: { src: string; alt: string }[];
+}
+
+export const Heading: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <div className="text-2xl font-semibold">{children}</div>;
+};
+
+export const Text: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <div className="opacity-85 font-medium leading-relaxed">{children}</div>;
+};
+
+export const Picture: React.FC<ImageProps> = ({ data, className, ...props }) => {
+  return ( 
+    <div className="border-4 border-yellow-800">
+      <img src={data[0].src} alt={data[0].alt} className="opacity-10"/>
+    </div>
+  );
+};
+
+export const Divider: React.FC = () => {
+  return <div className="pt-6 border-b border-neutral-700" />;
+};
---
+++ astro-pop/src/content/works/accredit.ts
@@ -0,0 +1,267 @@
+export const accredit = {
+  // Metadata
+  name: "Accredit",
+  href: "/work/accredit",
+  accentBg: "bg-green-50",
+  tags: ["Finance", "Mobile App"],
+  description: "Building infrastructure that powers the new age of AI agents for enterprise customers",
+  images: ["/accredit/logo.webp", "", ""],
+
+  // case study content
+  sections: [
+    {
+      id: "overview",
+      header: "Overview",
+      content: [
+        {
+          type: "heading",
+          data: "Overview",
+        },
+        {
+          type: "text",
+          data: "Loans are as old as banks. They are risky but come in handy a lot of the time. They help support the average business during bad months and help individuals to overcome dire situations. In today’s world, quick instant loans are the go-to as more people turn to digital solutions for their financial needs and with Accredit, we are taking a new approach to elevate the consumer lending experience.",
+        },
+        {
+          type: "image",
+          data: [{ src: "/accredit/f-7.webp", alt: "Leading showcase", loading: "eager" }],
+        },
+        {
+          type: "text",
+          data: "I led the design of the brand identity and the first version of the application to be launched on mobile platforms.",
+        },
+        {
+          type: "info-grid",
+          data: [
+            { label: "ROLE", value: "Design Lead" },
+            { label: "DURATION", value: "6 weeks" },
+            { label: "WHAT I DID", value: ["Branding", "Product Strategy", "Mobile Application Design", "Usability Testing"] },
+          ],
+        },
+        { type: "divider" },
+      ],
+    },
+    {
+      id: "evaluation-and-research",
+      header: "Evaluation and Research",
+      content: [
+        {
+          type: "image",
+          data: [{ src: "/accredit/f-5.webp", alt: "Leading showcase" }],
+        },
+        {
+          type: "heading",
+          data: "The challenge",
+        },
+        {
+          type: "text",
+          data: "There has been a significant shift in the ease of getting loans thanks to private lenders however this of course came with its problems. Several loan apps use unethical practices, impose exorbitant interest rates and are not self-serviceable.",
+        },
+        {
+          type: "text",
+          data: "The goal of this engagement was to refine the loan application and repayment process as well as design an identity that drives growth with trust and credibility.",
+        },
+        {
+          type: "heading",
+          data: "Research - User"
+        },
+        {
+          type: "quote",
+          data: "\“What has been your experience with loan apps?\”",
+        },
+        {
+          type: "text",
+          data: "Conversations with active borrowers and responses from participants helped validate the client’s mission. Going a step further, I read forums, articles and tweets to analyze general sentiment on loan apps. This comprehensive approach gave an in-depth understanding of the needs and frustrations of borrowers.",
+        },
+        {
+          type: "stat-grid",
+          data: [
+            { label: "calls", value: "4" },
+            { label: "in-person interview", value: "1" },
+            { label: "survey responses", value: "12" },
+            { label: "online resources", value: "20+" },
+          ]
+        },
+        {
+          type: "image",
+          data: [{ src: "/accredit/f-6.webp", alt: "Feedback on stickies" }],
+        },
+        {
+          type: "heading",
+          data: "Research - Market",
+        },
+        {
+          type: "quote",
+          data: "\“How do market leaders differentiate from the rest?\”",
+        },
+        {
+          type: "image",
+          data: [{ src: "/accredit/competitors.webp", alt: "Competitors" }],
+        },
+        {
+          type: "text",
+          data: "To get a good grasp of the industry landscape, I began by gathering information on 4 dominant competitors and a dozen other lenders, on how they use competitive differentiation to their advantage. These include their launch dates, product offerings, as well as their customer’s feedback and unmet needs. Working together with the client on this was crucial to identify market opportunities and gaps to exploit.",
+        },
+        { type: "heading", data: "Key Findings" },
+        {
+          type: "card",
+          data: [
+            {
+              label: "Repayments",
+              value: "Many people cited confusion over repayment options and displeasure with aggressive tactics used by some lenders.",
+            },
+          ],
+        },
+        {
+          type: "card",
+          data: [
+            {
+              label: "User Experience",
+              value: "Borrowers have no clue why their loan application was not approved, slow signup process with cumbersome requirements.",
+            },
+          ],
+        },
+        {
+          type: "card",
+          data: [
+            {
+              label: "High Interest Rates",
+              value: "Borrowers are troubled by exorbitant interest rates of major lenders, often leading to debt traps and financial stress.",
+            },
+          ],
+        },
+        {
+          type: "image",
+          data: [{
+            src: "/accredit/f-4.webp",
+            alt: "User Persona",
+          }],
+        },
+        {
+          type: "text",
+          data: "In order to empathize with users on a deeper level, I created 2 personas that represent potential users of Accredit - Cynthia as a returning borrower and Jamal as the newbie, each with their unique backgrounds and goals. This helped me to approach the next-phase of problem solving with a focus on the users and also throughout the entire design process.",
+        },
+        { type: "heading", data: "Possible solutions?" },
+        {
+          type: "card",
+          data: [{
+            label: "- Repayments",
+            value: "Borrowers have options to repay loans even when offline. The app should be self-serviceable to help users recover from loan default or disapproval.",
+          }],
+        },
+        {
+          type: "card",
+          data: [{
+            label: "- User Experience",
+            value: "Ingenious branding and a product with clear communication, accessible design and ease of use optimized with iterations.",
+          }],
+        },
+        {
+          type: "card",
+          data: [{
+            label: "- Credit System",
+            value: "A credit-oriented approach to loan disbursement and privileges encouraging users to build financial trustworthiness.",
+          }],
+        },
+        { type: "divider" },
+      ],
+    },
+    {
+      id: "design-and-iteration",
+      header: "Design and Iteration",
+      content: [
+        { type: "heading", data: "Planning" },
+        {
+          type: "text",
+          data: "Moving forward, I outlined the end-to-end structure of the product by organizing pages, features, and functionalities logically in a workflow. This ensures newbies like Jamal will be able to navigate the app seamlessly and without confusion. The result of this process was a view of the application that was not only holistic but also meticulous in detail.",
+        },
+        {
+          type: "image",
+          data: [{
+            src: "/accredit/f-14.webp",
+            alt: "Information architecture",
+          }],
+        },
+        { type: "heading", data: "Styles and components" },
+        {
+          type: "text",
+          data: "Circling back on the canvas, the goal for the brand identity was simplicity and continuity. From ideating on various logo ideas to foundational visual elements, the client made their pick. Accredit - your bridge to credit, positioned to be warm, bold, and to communicate with a direct tone.",
+        },
+        {
+          type: "image",
+          data: [{
+            src: "/accredit/f-3.webp",
+            alt: "Branding & UI Kit",
+          }],
+        },
+        { type: "heading", data: "Iterations" },
+        {
+          type: "text",
+          data: "Recall, a product like this had to be easy to use, accessible, and self-serviceable - one way to achieve this was iterative design. For instance, on the home screen layout, the thumb zone proved to be the most intuitive, informing the use of a floating action button to initiate the repayment process.",
+        },
+        {
+          type: "image",
+          data: [{ src: "/accredit/f-2.webp", alt: "Home Page Iterations" }],
+        },
+        { type: "heading", data: "Usability Study" },
+        {
+          type: "text",
+          data: "Another hurdle was to reduce drop-off and hesitation in the last step of the loan application flow. We noticed participants often read the agreement in desperation as it was a key requirement for approval. To solve this, the secure PIN was used as a universal authenticator on the app and the terms were instead linked on the page.",
+        },
+        {
+          type: "text",
+          data: " A resulting unmoderated testing session showed that they all completed the flow in less time and with relative ease on the updated design.",
+        },
+        {
+          type: "image-split",
+          data: [
+            {
+              src: "/accredit/f-9.webp",
+              alt: "Before Study",
+            },
+            {
+              src: "/accredit/f-10.webp",
+              alt: "After Study",
+            },
+          ],
+        },
+        { type: "divider" },
+      ],
+    },
+    {
+      id: "final-solution",
+      header: "Final Solution",
+      content: [
+        {
+          type: "heading",
+          data: "Solution",
+        },
+        {
+          type: "text",
+          data: "The design process embraces a human-centered approach, this means intensive research, conducting usability testing and iterating on feedback to ensure a pleasant experience every time users come on the app. This all contributed to a product with a user-friendly interface, strong visual appeal, and seamless functionality that resonates with borrowers.",
+        },
+        {
+          type: "image",
+          data: [{
+            src: "/accredit/f-1.webp",
+            alt: "KYC Flow",
+          }],
+        },
+        { type: "divider" },
+      ],
+    },
+    {
+      id: "learnings",
+      header: "Learnings",
+      content: [
+        {
+          type: "heading",
+          data: "Key Takeaways",
+        },
+        {
+          type: "text",
+          data: "This project came with its challenges but thorough research and testing informed the design process and ensured a product that meets the users needs. The branding and design system establishes trust and recognition. Together with the user experience and focus on error handling, Accredit is set on a path for sustainable growth in the market.",
+        },
+      ],
+    },
+  ],
+};

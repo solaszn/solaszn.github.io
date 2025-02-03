2289124eb42b12c00bc855de02304afc6bfb9c87
solaszn <olawuyi.sola@yahoo.com>
2025-02-03 16:47:05 +0100

    update atrium content

--- src/content/works/reframe.ts
+++ src/content/works/reframe.ts
@@ -27,20 +27,20 @@ export const reframe = {
         },
         {
           type: "image",
-          data: [{ src: "/reframe/dev-page.webp", alt: "Developer Landing Page" }],
+          data: [{ src: "/reframe/f-1.webp", alt: "Developer Landing Page" }],
         },
         { type: "heading", data: "The Background" },
         {
           type: "text",
-          data: "Reframe started as a suite of AI-powered tools helping teams like Empowerly and JLL streamline data research and enrichment workflows with agentic AI and increase productivity.",
+          data: "For our main product Reframe AI, we had already developed and launched a product suite that helped top teams at Empowerly and JLL streamline data research with agentic AI workflows. We integrated AI agents into dataframes within a spreadsheet-like interface, allowing customers to automate data enrichment and qualification. \"A spreadsheet that fills itself\" - a competitors tagline",
         },
         {
           type: "image",
-          data: [{ src: "/reframe/data-frame.webp", alt: "Data Frame And Entity Recognition" }],
+          data: [{ src: "/reframe/f-2.webp", alt: "Data Frame And Entity Recognition" }],
         },
         {
           type: "text",
-          data: "We also built a product similar to Notion Forms and Atlassian forms, to sync data to a dataframe and then run automated agentic workflows to enrich that data.",
+          data: "Beyond this, we also built a system that let clients sync their data realtime into a dataframe and run automated workflows to refine it. Reframe AI Forms, seamlessly integrated with customer stack and existing tools, feeding form submissions directly into dataframes to trigger advanced workflows. Collectively, we managed to take this feature from prototype to production in just three weeks; faster than teams at Notion Forms and Asana Forms.",
         },
         {
           type: "video",
@@ -56,27 +56,55 @@ export const reframe = {
         { type: "heading", data: "The Problem" },
         {
           type: "text",
-          data: "However, deeper research revealed that most teams used Reframe to gather intelligence on people and companies. It became clear that new users struggled to see value without assistance, and research workflows were not fully optimized.",
+          data: "Over time, our research revealed that most teams used the product to gather intelligence on people and companies. When working with large dataframes (10k+ records) for instance, users often needed to dive deeper into a subset of high-value leads or entities. However, we did not provide a siloed environment or dedicated space for that, leaving users to export data elsewhere. Given that data has gravity, users tend to stick to the platform that meets most of their needs, so we were losing customers because we were restricting them.",
         },
         {
           type: "image",
-          data: [{ src: "/reframe/data-frame.webp", alt: "Data Frame Example" }],
+          data: [{ src: "/reframe/js.webp", alt: "How people used the product" }],
+        },
+        {
+          type: "text",
+          data: "Another key issue that became glaring was that, new users struggled to immediately see value in the platform, and research workflows were not fully optimized even for advanced users. We needed a better way to support customers and enhance their experience. Therefore we decided to rethink our strategy and how we meet our customers growing needs, starting with our primary problem statement.",
+        },
+        {
+          type: "quote",
+          data: "\"How can we make it easier and seamless for teams to quickly research hundreds of data points on people and companies without leaving our platform?\"",
         },
         {
           type: "heading",
-          data: "Problem Statements",
+          data: "Gauging Customer Interest",
         },
         {
           type: "text",
-          data: "\"How can we make it easier for teams to waste less time and resources researching leads and focus their energy on selling?\"",
-        },
-        {
-          type: "text",
-          data: "\"How can we enable teams to research hundreds of data points on people and companies seamlessly?\"",
+          data: "To ensure we were going in the right direction, we identified and segmented key customer factions based on how they engaged with the product. This helped us pinpoint opportunities, competitive gaps, and areas for quick wins. We prioritized users whose problems closely aligned with our solution, ensuring that the product’s value would be immediately clear - the Aha! moment; upon using it.",
         },
         {
           type: "image",
           data: [{ src: "/reframe/audience.webp", alt: "Target Audience Segmentation" }],
+        },
+        {
+          type: "heading",
+          data: "Market Research - Competitors?",
+        },
+        {
+          type: "text",
+          data: "Analyzing the competitive landscape, we identified several companies offering similar data solutions. Instead of competing directly, we focused on differentiation by creating a suite of tools designed for data-heavy teams.",
+        },
+        {
+          type: "image",
+          data: [{ src: "/reframe/competitors.webp", alt: "Competitors" }],
+        },
+        {
+          type: "heading",
+          data: "Integrating User Scenarios",
+        },
+        {
+          type: "text",
+          data: "To ensure we stayed true to the customers painpoints throughout the process, we used real-world scenarios and test cases as guiding anchors. This frmework helped sharpen and influence our decision making, putting users front and center by design. This also helped shape the product by catering to the evolving needs of active customers while also considering potential teams we could pitch to.",
+        },
+        {
+          type: "image",
+          data: [{ src: "/reframe/scenarios.webp", alt: "User examples" }],
         },
         { type: "divider" },
       ],
